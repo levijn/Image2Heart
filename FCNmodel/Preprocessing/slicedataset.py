4f16a144372cf553f47b931aeb6702b39a46d6dd
levijn <levijndejager@gmail.com>
2021-12-13 10:32:26 +0100

    Dataset and loader
    Dataset now resizes the images.
    Started with the dataloader.

--- FCNmodel/Preprocessing/slicedataset.py
+++ FCNmodel/Preprocessing/slicedataset.py
@@ -1,7 +1,14 @@
+"""
+Custom dataset class used by the DataLoader class.
+"""
+
 import os
 import sys
 import inspect
+import numpy as np
+from torch.utils import data
 
+# Import the path of different folders
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 sys.path.append(os.path.dirname(current_dir))
 
@@ -9,8 +16,8 @@ import config
 from preprocess import (get_filenames, 
                         create_indexed_file_dict,
                         load_slice_array,
-                        plot_slice_with_lbl)
-from torch.utils import data
+                        plot_slice_with_lbl,
+                        get_all_shapes_hw)
 
 
 class SliceDataset(data.Dataset):
@@ -27,27 +34,81 @@ class SliceDataset(data.Dataset):
         self.data_dir = data_dir
         self.idx_dict = idx_dict
         self.transform = transform
-        
+    
     def __len__(self):
         return len(self.idx_dict)
 
     def __getitem__(self, idx):
+        #get filenames
         slice = self.idx_dict[idx]
         img_data_file = slice["img_data_file"]
         lbl_data_file = slice["lbl_data_file"]
         
+        #load files
         img_array = load_slice_array(os.path.join(self.data_dir, img_data_file))
         lbl_array = load_slice_array(os.path.join(self.data_dir, lbl_data_file))
         
-        return img_array, lbl_array
+        org_size = img_array.shape[:2]
+        print(org_size)
+        
+        sample = {"image": img_array,
+                  "label": lbl_array,
+                  "size": org_size}
+        
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
+
+
+class PadImage(object):
+    """Adds padding to the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size: tuple):
+        assert isinstance(output_size, tuple)
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label, size = sample["image"], sample["label"], sample["size"]
+
+        h, w = size
+        new_h, new_w = self.output_size
+        h_pad = new_h - h
+        w_pad = new_w - w
+        
+        new_image = np.pad(image, ((0,h_pad), (0,w_pad)), "constant", constant_values=(0,0))
+        new_label = np.pad(label, ((0,h_pad), (0,w_pad)), "constant", constant_values=(0,0))
+        
+        return {"image": new_image, "label": new_label, "size": size}
 
 
 def main():
     array_path = os.path.join(config.data_dir, "slice_arrays")
+    
     data_dict = create_indexed_file_dict(array_path)
-    slicedataset = SliceDataset(array_path, data_dict)
-    img_array, lbl_array = slicedataset[40]
-    plot_slice_with_lbl(img_array, lbl_array)
+    
+    # Use to calculate h and w for the image padding. Only run again if data changes.
+    # heights, widths = get_all_shapes_hw(array_path, data_dict)
+    # print(max(heights), max(widths))
+    
+    padding = 428, 512
+    
+    padder = PadImage(padding)
+    slicedata = SliceDataset(array_path, data_dict, transform=padder)
+
+    dataloader = data.DataLoader(slicedata, batch_size=4, shuffle=True, num_workers=0)
+    
+    for i_batch, sample_batched in enumerate(dataloader):
+        print(i_batch, sample_batched['image'].size(),
+          sample_batched['label'].size(),
+          sample_batched["size"])
+    
     
 
 
