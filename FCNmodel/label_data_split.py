f0fb55c127bfe383ab586a8d530f73475e27e621
Daan1289 <Daanscherrenburg@gmail.com>
2021-12-14 12:44:43 +0100

    Created function label_data_split

--- FCNmodel/label_data_split.py
+++ FCNmodel/label_data_split.py
@@ -4,21 +4,23 @@ import config
 import sys
 import inspect
 
-parent_path = config.data_dir       #path to the main "Data" folder
-folder_path = os.path.join(parent_path, "simpledata")       #path to current location of the files
+def label_data_split(location, data_destination, label_destination):
 
-files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    parent_path = config.data_dir       #path to the main "Data" folder
+    folder_path = os.path.join(parent_path, location)       #path to current location of the files
 
-for file in files:
-    if "gt" in file:
-        folder_name = "training_labels"
-    else:
-        folder_name = "training_data"
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-    new_path = os.path.join(parent_path, folder_name)
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
+    for file in files:
+        if "gt" in file:
+            folder_name = label_destination
+        else:
+            folder_name = data_destination
 
-    old_image_path = os.path.join(folder_path, file)
-    new_image_path = os.path.join(new_path, file)
-    shutil.copy(old_image_path, new_image_path)
+        new_path = os.path.join(parent_path, folder_name)
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+
+        old_image_path = os.path.join(folder_path, file)
+        new_image_path = os.path.join(new_path, file)
+        shutil.copy(old_image_path, new_image_path)
