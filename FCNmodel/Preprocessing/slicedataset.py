6b069a814b4afaaabedc1b835ccc47b8b17b1838
levijn <levijndejager@gmail.com>
2021-12-14 16:38:10 +0100

    start gemaakt met batches aanpassen

--- FCNmodel/Preprocessing/slicedataset.py
+++ FCNmodel/Preprocessing/slicedataset.py
@@ -154,12 +154,12 @@ def main():
 
     plot_slice_with_lbl(slice["image"][1,:,:], slice["label"])
     
-    # dataloader = data.DataLoader(slicedata, batch_size=8, shuffle=True, num_workers=8)
+    dataloader = data.DataLoader(slicedata, batch_size=1, shuffle=True, num_workers=8)
     
-    # for i_batch, sample_batched in enumerate(dataloader):
-    #     print(i_batch, sample_batched['image'].size(),
-    #       sample_batched['label'].size(),
-    #       sample_batched["size"])
+    for i_batch, sample_batched in enumerate(dataloader):
+        print(i_batch, sample_batched['image'].size(),
+          sample_batched['label'].size(),
+          sample_batched["size"])
     
 
 if __name__ == '__main__':
