8d2782f1303db141d197b1dee5d342e6b8c9a504
tedruiter <tedruiter@gmail.com>
2021-12-14 13:38:58 +0100

    added dataloaders
    added dtaloaders voor training and validation set.  this dictionary is still empty, but if the paths are set to the correct folder, it should work

--- FCNmodel/Model/model_training.py
+++ FCNmodel/Model/model_training.py
@@ -23,28 +23,79 @@ from torchvision.utils import make_grid
 from torchvision.io import read_image
 from pathlib import Path
 
-
-
+# making directories
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-
 sys.path.append(os.path.dirname(current_dir))
 
-import config
+from config import data_dir, preprocessing_dir
+sys.path.append(preprocessing_dir)
 
-data_dir = config.data_dir
-preprocessing_dir = config.preprocessing_dir
 arrays_dir = os.path.join(data_dir, "slice_arrays")
-images_dir = os.path.join(data_dir, "slice_images")
-print(arrays_dir, images_dir)
-
-sys.path.insert(1, preprocessing_dir)
-
-from preprocess import plot_slice_with_lbl
-from slicedataset import main
 
 
+# veel te lang mee zitten kloten om dit uit preprocess.py te halen dus nu maar gekopieerd
+def get_filenames(directory) -> list:
+    """Returns a list of all the filenames in the directory"""
+    for (_, _, filenames) in os.walk(directory):
+        return filenames
 
 
+# inserting data
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+data_transforms = {
+    'train':
+    transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ]),
+    'validation':
+    transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        normalize
+    ]),
+}
+
+
+# pathing for training files and validation files aren't made, so this won't yet (dictionaries are empty)
+image_datasets = {
+    'train': 
+    get_filenames(os.path.join(arrays_dir, "testing_set")),
+    'validation': 
+    get_filenames(os.path.join(arrays_dir, "validation_set"))
+}
+
+
+dataloaders = {
+    'train':
+    torch.utils.data.DataLoader(image_datasets['train'],
+                                batch_size=32,
+                                shuffle=True,
+                                num_workers=0), 
+    'validation':
+    torch.utils.data.DataLoader(image_datasets['validation'],
+                                batch_size=32,
+                                shuffle=False,
+                                num_workers=0)
+}
+
+
+# creating network
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-print(data_dir)
+model = models.resnet50(pretrained=True).to(device)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.fc = nn.Sequential(
+               nn.Linear(2048, 128),
+               nn.ReLU(inplace=True),
+               nn.Linear(128, 2)).to(device)
+
+# train model
