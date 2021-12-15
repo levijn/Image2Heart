4aad9ccdff7e8a8cd4a60975a8e2829da620d6a7
Daan1289 <Daanscherrenburg@gmail.com>
2021-12-15 14:24:25 +0100

    Used dataloading image as imput for the untrained model

--- FCNmodel/Preprocessing/slicedataset.py
+++ FCNmodel/Preprocessing/slicedataset.py
@@ -36,6 +36,8 @@ class SliceDataset(data.Dataset):
         super().__init__()
         self.data_dir = data_dir
         self.idx_dict = idx_dict
+        keys = self.idx_dict.keys()
+        self.start_index = min(keys)
         self.transform = transform
     
     def __len__(self):
@@ -43,7 +45,7 @@ class SliceDataset(data.Dataset):
 
     def __getitem__(self, idx):
         #get filenames
-        slice = self.idx_dict[idx]
+        slice = self.idx_dict[idx+self.start_index]
         img_data_file = slice["img_data_file"]
         lbl_data_file = slice["lbl_data_file"]
         
@@ -163,45 +165,124 @@ class RandomZoom(object):
         
         return {"image": new_image, "label": new_label, "size": np.asarray([new_h, new_w])}
 
+class Dataloading:
+    """Creates two dataloaders. A train and test dataloader.
+    Args:
+        test_size: fraction of data used for testing.
+        array_path: path to the folder containing the arrayfiles per slice.
+        max_zoom: the maximum amount of zoom
+        padding: the size of the largest image
+        batch_size: size of the batches
+        shuffle: "True" to enable shuffle, "False" to disable shuffle
+    """
+
+    def __init__(self, test_size, array_path, max_zoom=10, padding=(428, 512), batch_size=4, shuffle=False) -> None:
+        self.test_size = test_size
+        self.array_path = array_path
+        self.max_zoom = max_zoom
+        self.padding = padding
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        
+        self.create_dicts()
+        self.create_transforms()
+        self.create_dataloaders()
+
+    def create_dicts(self):
+        self.data_dict = create_indexed_file_dict(self.array_path)
+        self.train_data_dict = {key: self.data_dict[key] for i, key in enumerate(self.data_dict.keys()) if i < (1-self.test_size)*len(self.data_dict)}
+        self.test_data_dict = {key: self.data_dict[key] for i, key in enumerate(self.data_dict.keys()) if i >= (1-self.test_size)*len(self.data_dict)}
+
+    def create_transforms(self):
+        randomzoom = RandomZoom(self.max_zoom)
+        padder = PadImage(self.padding)
+        sudorgb_converter = SudoRGB()
+        to_tensor = ToTensor()
+        encoder = OneHotEncoder()
+        self.composed_transform = transforms.Compose([padder, sudorgb_converter, to_tensor])
+    
+    def create_dataloaders(self):
+        self.train_slicedata = SliceDataset(self.array_path, self.train_data_dict, transform=self.composed_transform)
+        self.test_slicedata = SliceDataset(self.array_path, self.test_data_dict, transform=self.composed_transform)
+
+        self.train_dataloader = data.DataLoader(self.train_slicedata, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=8)
+        self.test_dataloader = data.DataLoader(self.test_slicedata, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=8)
+    
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
+    @staticmethod
+    def remove_padding(batch) -> list:
+        """Removes the padding from a batch and returns them as a list of dictionaries.
+        The batch will not be a stacked anymore.\n
+        Args:
+            batch: a single batch loaded from a dataloader using the SliceDataset.
+        """
+    
+        img_b, lbl_b, size_b = batch["image"], batch["label"], batch["size"]
+        samples = []
+        pad_deleter = RemovePadding()
+        for i in range(img_b.size(dim=0)):
+            sample = {"image": img_b[i,:,:,:], "label": lbl_b[i,:,:], "size": size_b[i,:]}
+            samples.append(pad_deleter(sample))
+        return samples
+    
+
+
 def main():
     array_path = os.path.join(config.data_dir, "slice_arrays")
     
     data_dict = create_indexed_file_dict(array_path)
 
-    test_size = 0.2
+    dataloading = Dataloading(0.2, array_path)
 
-    train_data_dict = {key: data_dict[key] for i, key in enumerate(data_dict.keys()) if i < (1-test_size)*len(data_dict)}
-    test_data_dict = {key: data_dict[key] for i, key in enumerate(data_dict.keys()) if i >= (1-test_size)*len(data_dict)}
+    onefile = ""
+    
+    for i_batch, sample_batched in enumerate(dataloading.test_dataloader):
+        # print(i_batch, sample_batched['image'].size(),
+        #    sample_batched['label'].size(),
+        #    sample_batched["size"])
+        onefile = sample_batched
+
+    print(onefile["image"])
+
+    # test_size = 0.2
+
+    # train_data_dict = {key: data_dict[key] for i, key in enumerate(data_dict.keys()) if i < (1-test_size)*len(data_dict)}
+    # test_data_dict = {key: data_dict[key] for i, key in enumerate(data_dict.keys()) if i >= (1-test_size)*len(data_dict)}
     
     
     # Use to calculate h and w for the image padding. Only run again if data changes.
     # heights, widths = get_all_shapes_hw(array_path, data_dict)
     # print(max(heights), max(widths))
     
-    padding = 428, 512
+    # padding = 428, 512
     
-    randomzoom = RandomZoom(20)
-    padder = PadImage(padding)
-    sudorgb_converter = SudoRGB()
-    remove_padding = RemovePadding()
-    to_tensor = ToTensor()
-    encoder = OneHotEncoder()
-    composed_transform = transforms.Compose([sudorgb_converter, to_tensor])
-    composed_zoomtransform = transforms.Compose([randomzoom, sudorgb_converter, to_tensor])
+    # randomzoom = RandomZoom(20)
+    # padder = PadImage(padding)
+    # sudorgb_converter = SudoRGB()
+    # remove_padding = RemovePadding()
+    # to_tensor = ToTensor()
+    # encoder = OneHotEncoder()
+    # composed_transform = transforms.Compose([sudorgb_converter, to_tensor])
+    # composed_zoomtransform = transforms.Compose([randomzoom, sudorgb_converter, to_tensor])
 
 
-    train_slicedata = SliceDataset(array_path, train_data_dict, transform=composed_transform)
-    test_slicedata = SliceDataset(array_path, test_data_dict, transform=composed_transform)
+    # train_slicedata = SliceDataset(array_path, train_data_dict, transform=composed_transform)
+    # test_slicedata = SliceDataset(array_path, test_data_dict, transform=composed_transform)
     
-    slicedata = SliceDataset(array_path, data_dict, transform=composed_transform)
-    zoomslicedata = SliceDataset(array_path, data_dict, transform=composed_zoomtransform)
-    slice = slicedata[1]
-    zoom_slice = zoomslicedata[600]
-    print(slice["image"].shape, zoom_slice["image"].shape)
+    # slicedata = SliceDataset(array_path, data_dict, transform=composed_transform)
+    # zoomslicedata = SliceDataset(array_path, data_dict, transform=composed_zoomtransform)
+    # slice = slicedata[1]
+    # zoom_slice = zoomslicedata[600]
+    # print(slice["image"].shape, zoom_slice["image"].shape)
 
-    plot_slice_with_lbl(zoom_slice["image"][1,:,:], zoom_slice["label"])
+    # plot_slice_with_lbl(zoom_slice["image"][1,:,:], zoom_slice["label"])
     
-    dataloader = data.DataLoader(slicedata, batch_size=1, shuffle=False, num_workers=8)
+    # dataloader = data.DataLoader(slicedata, batch_size=1, shuffle=False, num_workers=8)
     
     # for i_batch, sample_batched in enumerate(dataloader):
     #     print(i_batch, sample_batched['image'].size(),
--- FCNmodel/zooi/modeltest.py
+++ FCNmodel/zooi/modeltest.py
@@ -6,6 +6,7 @@ import inspect
 import numpy as np
 import PIL
 import matplotlib.pyplot as plt
+import sys
 
 import torchvision.transforms.functional as F
 from torchvision.transforms.functional import convert_image_dtype, resize
@@ -15,71 +16,75 @@ from torchvision.utils import make_grid
 from torchvision.io import read_image
 from pathlib import Path
 
+
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
+prepdir = os.path.join(parentdir, "Preprocessing")
+sys.path.append(prepdir)
+sys.path.append(parentdir)
 
-convert_tensor = transforms.ToTensor()
+import config
+from slicedataset import Dataloading
 
-plt.rcParams["savefig.bbox"] = 'tight'
+def main():
+    array_path = os.path.join(config.data_dir, "slice_arrays")
+    dataloading = Dataloading(0.2, array_path)
 
-dog_int = read_image(os.path.join(currentdir, 'dog.jpg'))
-dog2_int = read_image(os.path.join(currentdir, 'dog2.png'))
-boat_int = read_image(os.path.join(currentdir, 'boat.jpg'))
-vinc_int = read_image(os.path.join(currentdir, 'Vincent.png'))
+    onebatch = ""
+    for i_batch, sample_batched in enumerate(dataloading.test_dataloader):
+        onefile = sample_batched
+        break
 
-vinc_rgba = PIL.Image.open(os.path.join(currentdir, 'Vincent.png'))
-vinc_rgb = vinc_rgba.convert('RGB')
-vinc_new = convert_tensor(vinc_rgb)
+    samples = Dataloading.remove_padding(onefile)
+    image = samples[0]["image"]
 
-print(dog2_int.shape)
-
-
-#Used to plot the images:
-dog = dog_int.detach()
-dog2 = dog2_int.detach()
-dog = F.to_pil_image(dog)
-dog_array = np.asarray(dog)
-dog2 = F.to_pil_image(dog2)
-dog2_array = np.asarray(dog2)
-
-batch_int = torch.stack([vinc_new])
-batch = convert_image_dtype(batch_int, dtype=torch.float)
-
-fcn = fcn_resnet50(pretrained=True)
-# print(fcn)
-fcn = fcn.eval()
-
-print(batch)
-normalized_batch = F.normalize(batch, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
-print(normalized_batch.shape)
-
-output = fcn(normalized_batch)["out"]
-
-normalized_masks = torch.nn.functional.softmax(output, dim=1)
-
-for i in range(normalized_masks.shape[1]):
-    img = F.to_pil_image(normalized_masks[0,i,:,:])
+    img = F.to_pil_image(image[1,:,:])
     plt.imshow(img)
     plt.show()
 
-sem_classes = [
-    '__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
-    'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
-]
-sem_class_to_idx = 'person'
-#{cls: idx for (idx, cls) in enumerate(sem_classes)}
+
+    plt.rcParams["savefig.bbox"] = 'tight'
+
+    # dog_int = read_image(os.path.join(currentdir, 'dog.jpg'))
+    # dog2_int = read_image(os.path.join(currentdir, 'dog2.png'))
+    # boat_int = read_image(os.path.join(currentdir, 'boat.jpg'))
+    # vinc_int = read_image(os.path.join(currentdir, 'Vincent.png'))
+
+    # vinc_rgba = PIL.Image.open(os.path.join(currentdir, 'Vincent.png'))
+    # vinc_rgb = vinc_rgba.convert('RGB')
+    # vinc_new = convert_tensor(vinc_rgb)
+
+    # print(dog2_int.shape)
 
 
-dog_and_boat_masks = [
-    normalized_masks[img_idx, sem_class_to_idx[cls]]
-    for img_idx in range(batch.shape[0])
-    for cls in ('dog', 'boat')
-]
+    #Used to plot the images:
+    # dog = dog_int.detach()
+    # dog2 = dog2_int.detach()
+    # dog = F.to_pil_image(dog)
+    # dog_array = np.asarray(dog)
+    # dog2 = F.to_pil_image(dog2)
+    # dog2_array = np.asarray(dog2)
 
-dog_mask = dog_and_boat_masks[1]
-dog_mask = dog_mask.detach()
-dog_mask = F.to_pil_image(dog_mask)
-dog_array = np.asarray(dog_mask)
-plt.imshow(dog_array)
-plt.show()
+    batch_int = torch.stack([image])
+    batch = convert_image_dtype(batch_int, dtype=torch.float)
+
+    fcn = fcn_resnet50(pretrained=True)
+    # print(fcn)
+    fcn = fcn.eval()
+
+    print(batch)
+    normalized_batch = F.normalize(batch, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    print(normalized_batch.shape)
+
+    output = fcn(normalized_batch)["out"]
+
+    normalized_masks = torch.nn.functional.softmax(output, dim=1)
+
+    for i in range(normalized_masks.shape[1]):
+        img = F.to_pil_image(normalized_masks[0,i,:,:])
+        plt.imshow(img)
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
