d4c4275d84286ccd8c9e65c6f29d63e6f93f3591
levijn <levijndejager@gmail.com>
2021-12-16 12:26:07 +0100

    function to change the headsize of the model

---
+++ FCNmodel/Model/change_head.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import inspect
+
+import torch
+from torchvision.models.segmentation import fcn_resnet50
+from torchvision.models.segmentation.fcn import FCNHead
+
+
+def change_headsize(model, num_classes):
+    new_classifier = FCNHead(2048, num_classes)
+    new_aux_classifier = FCNHead(1024, num_classes)
+    model._modules["classifier"] = new_classifier
+    model._modules["aux_classifier"] = new_classifier
+    return model
+    
+    
+
+def main():
+    fcn = fcn_resnet50(pretrained=True)
+    change_headsize(fcn, 4)
+
+if __name__ == '__main__':
+    main()
