744fad78dd10e15b20e628a3377d82d8414a3c39
levijn <levijndejager@gmail.com>
2021-12-16 21:08:12 +0100

    learning rate

--- FCNmodel/Model/finetune_learnrate.py
+++ FCNmodel/Model/finetune_learnrate.py
@@ -5,15 +5,9 @@ import numpy as np
 import PIL
 import matplotlib.pyplot as plt
 import sys
-from pathlib import Path
 
-from torchvision import transforms
 from torchvision.models.segmentation import fcn_resnet50
 import torchvision.transforms.functional as F
-from torchvision.transforms.functional import convert_image_dtype, resize
-from torchvision.transforms import Resize
-from torchvision.utils import make_grid
-from torchvision.io import read_image
 
 #adding needed folderpaths
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -24,11 +18,21 @@ sys.path.append(parentdir)
 
 #importing needed files
 import config
+from preprocess import save_slice_array, load_slice_array
 from slicedataset import Dataloading
 from change_head import change_headsize
 
 
-def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=0.001, pretrained=True, shuffle=True, array_path=config.array_dir, num_classes=4):
+def plot_learningrate(learningrates_loss):
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111)
+    for lrloss in learningrates_loss:
+        ax.plot(lrloss)
+    plt.savefig(os.path.join(currentdir, "learningrate.png"))
+    plt.show()
+
+
+def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=[0.001], pretrained=True, shuffle=True, array_path=config.array_dir, num_classes=4):
     """Trains the model using the dataloader
     Args:
         test_size: fraction of data used for testing.
@@ -45,13 +49,15 @@ def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=0.0
     #creating fcn model
     fcn = fcn_resnet50(pretrained=pretrained)
 
+    # setting model to trainingmode and set the device
     fcn.train()
     device = "cuda"
     
-    # feezing its parameters
+    # freezing its parameters
     for param in fcn.parameters():
         param.requires_grad = False
     
+    # change head to output 4 classes
     fcn = change_headsize(fcn, 4).to(device)
 
     
@@ -62,34 +68,45 @@ def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=0.0
         p.numel() for p in fcn.parameters() if p.requires_grad)
     print(f'{total_trainable_params:,} training parameters.')
 
-    #looping through epochs
-    for epoch in range(num_epochs):
-        print(f"Epoch: {epoch}")
-        running_loss = 0.0
-        
-        #looping through batches in each epoch
-        for i_batch, batch in enumerate(dataloading.train_dataloader):
-            print(f"Batch: {i_batch}")
-
-            criterion = torch.nn.CrossEntropyLoss()
-            optimizer = torch.optim.Adam(fcn.parameters(), lr=learning_rate)
-
-            data = convert_image_dtype(batch["image"], dtype=torch.float).to(device)
-            target = batch["label"].to(device)
-            optimizer.zero_grad()
-            output = fcn(data)
-            loss = criterion(output["out"], target.long())
-            loss.backward()
-            optimizer.step()
-            
-            running_loss += loss.item()
-            if i_batch % 50 == 49:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i_batch + 1, running_loss / 50))
-                running_loss = 0.0
+    # initialize way to save loss
+    loss_per_lr= []
     
-    #saving calculated weights to "weights.h5"
-    torch.save(fcn.state_dict(), os.path.join(currentdir, "weights.h5"))
+    for LR in learning_rate:
+        loss_per_epoch = []
+        #looping through epochs
+        for epoch in range(num_epochs):
+            print(f"------ Learning rate: {LR} --> Epoch: {epoch+1} ------")
+            epoch_loss = 0.0
+            
+            #looping through batches in each epoch
+            for i_batch, batch in enumerate(dataloading.train_dataloader):
+
+                criterion = torch.nn.CrossEntropyLoss()
+                optimizer = torch.optim.Adam(fcn.parameters(), lr=LR)
+
+                data = F.convert_image_dtype(batch["image"], dtype=torch.float).to(device)
+                target = batch["label"].to(device)
+                optimizer.zero_grad()
+                output = fcn(data)
+                loss = criterion(output["out"], target.long())
+                loss.backward()
+                optimizer.step()
+                
+                epoch_loss += output["out"].shape[0] * loss.item()
+            
+            loss_per_epoch.append(epoch_loss/len(dataloading.train_slicedata))
+            print("Loss:", epoch_loss/len(dataloading.train_slicedata))
+            
+        loss_per_lr.append(loss_per_epoch)
+        #saving calculated weights to "weights.h5"
+        torch.save(fcn.state_dict(), os.path.join(currentdir, f"weights_lr{int(LR*1000)}.h5"))
+    
+    
+    plot_learningrate(loss_per_lr)
+    
+    
+
+
 
 def running_model(pretrained=False, num_classes=4):
     """Running the model and testing it on 1 sample
@@ -102,7 +119,7 @@ def running_model(pretrained=False, num_classes=4):
     #loading the weights from "weights.h5"
     device = "cuda"
     fcn = change_headsize(fcn, 4)
-    fcn.load_state_dict(torch.load(os.path.join(currentdir, "weights.h5")))
+    fcn.load_state_dict(torch.load(os.path.join(currentdir, "weights_lr1.h5")))
 
 
     plt.rcParams["savefig.bbox"] = 'tight'
@@ -110,13 +127,13 @@ def running_model(pretrained=False, num_classes=4):
     #retrieving 1 image for training
     one_batch = None
     dataloading = Dataloading(test_size=0.2, array_path=config.array_dir, batch_size=4, shuffle=True)
-    for i_batch, batch in enumerate(dataloading.test_dataloader):
+    for i_batch, batch in enumerate(dataloading.train_dataloader):
         #remove the padding
         one_batch = batch
         break
 
     sample = one_batch["image"]
-    sample = convert_image_dtype(sample, dtype=torch.float)
+    sample = F.convert_image_dtype(sample, dtype=torch.float)
 
     fcn.eval()
 
@@ -136,15 +153,20 @@ def running_model(pretrained=False, num_classes=4):
         plt.imshow(img)
         plt.show()
 
+
+
+
 def main():
     #set to True if the model has been trained with the weights stored at "weights.h5", False otherwise
-    trained = True
+    trained = False
 
     if trained is False:
-        training_model(pretrained=True)
-        running_model()
+        learningrates = [0.001, 0.01, 0.1, 1]
+        training_model(pretrained=True, learning_rate=learningrates, batch_size=16, num_epochs=50)
+        running_model(pretrained=True)
     elif trained is True:
         running_model(pretrained=True)
+    
 
 if __name__ == '__main__':
     main()
