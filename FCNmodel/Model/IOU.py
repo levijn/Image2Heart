2515f8ae6cdddfcb95e8431602e39586195fe76d
levijn <levijndejager@gmail.com>
2022-01-13 14:06:34 +0100

    nieuw bestand gemaakt en aan het trainen
    gebruik iou maar een kwart van de tijd

--- FCNmodel/Model/IOU.py
+++ FCNmodel/Model/IOU.py
@@ -6,6 +6,7 @@ import PIL
 import matplotlib.pyplot as plt
 import sys
 import tensorflow as tf
+import torch
 
 from torchvision.models.segmentation import fcn_resnet50
 import torchvision.transforms.functional as F
@@ -87,6 +88,46 @@ def IOU(label_stack, output_stack):
     print(f"Total iou of batch: {total_iou}")
     return total_iou
 
+# werkt niet
+# def jaccard(label_stack, output_stack):
+#     jac = IoU(num_classes=4)
+#     seg_output = convert_to_segmented_imgs(output_stack)
+#     seg_output_ts = []
+#     for img in seg_output:
+#         seg_output_ts.append(torch.tensor(img, dtype=torch.int32))
+#     pred = torch.stack(seg_output_ts, dim=0)
+#     target = label_stack.int()
+#     print(pred.dtype)
+#     print(target.dtype)
+#     jacindex = jac(output_stack, label_stack)
+#     return jacindex.item()
+    
+
+# SMOOTH = 1e-6
+
+# def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
+#     # You can comment out this line if you are passing tensors of equal shape
+#     # But if you are passing output from UNet or something it will most probably
+#     # be with the BATCH x 1 x H x W shape
+#     seg_output = convert_to_segmented_imgs(outputs)
+#     seg_output_ts = []
+#     for img in seg_output:
+#         seg_output_ts.append(torch.tensor(img, dtype=torch.int32))
+#     outputs = torch.stack(seg_output_ts)
+#     print(outputs.size())
+#     print(labels.size())
+    
+#     outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+    
+#     intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+#     union = (outputs | labels).float().sum((1, 2))         # Will be zzero if both are 0
+    
+#     iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+    
+#     thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+    
+#     return thresholded  # Or thresholded.mean() if you are interested in average across the batch
+
 
 def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=[0.001], pretrained=True, shuffle=True, array_path=config.array_dir, num_classes=4, savingfile="weights.h5"):
     """Trains the model using the dataloader
@@ -116,7 +157,6 @@ def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=[0.
     
     # change head to output 4 classes
     fcn = change_headsize(fcn, 4).to(device)
-
     
     # Find total parameters and trainable parameters
     total_params = sum(p.numel() for p in fcn.parameters())
@@ -137,6 +177,8 @@ def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=[0.
             print(f"------ Learning rate: {LR} --> Epoch: {epoch+1} ------")
             epoch_train_iou = 0.0
             epoch_eval_iou = 0.0
+            epoch_train_loss = 0.0
+            epoch_eval_loss = 0.0
             fcn.train()
             # Model training loop
             print("Going through training data....")
@@ -153,8 +195,9 @@ def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=[0.
                 loss.backward()
                 optimizer.step()
                 
-                epoch_train_iou += IOU(batch["label"], output["out"].detach())
-                
+                epoch_train_loss += output["out"].shape[0]*loss.item()
+                # epoch_train_iou += IOU(batch["label"], output["out"].detach())
+                print(iou_pytorch(output["out"], batch["label"]))
             print("Going through testing data....")
 
             # Calculate validation loss after training
@@ -164,17 +207,24 @@ def training_model(test_size=0.2, num_epochs=10, batch_size=4, learning_rate=[0.
                 data = F.convert_image_dtype(batch["image"], dtype=torch.float).to(device)
                 target = batch["label"].to(device)
                 output = fcn(data)
-
-                epoch_eval_iou += IOU(batch["label"], output["out"].detach())
+                loss = criterion(output["out"], target.long())
+                
+                epoch_eval_loss += output["out"].shape[0]*loss.item()
+                # epoch_eval_iou += IOU(batch["label"], output["out"].detach())
             
 
             train_iou = epoch_train_iou/len(dataloading.train_slicedata)
             eval_iou = epoch_eval_iou/len(dataloading.test_slicedata)
             print("Training IoU:", train_iou)
             print("Evaluation IoU:", eval_iou)
+            
+            train_loss = epoch_train_loss/len(dataloading.train_slicedata)
+            eval_loss = epoch_eval_loss/len(dataloading.test_slicedata)
+            print("Training loss:", train_loss)
+            print("Evaluation loss:", eval_loss)
 
-        train_loss_per_lr.append(train_loss_per_epoch)
-        eval_loss_per_lr.append(eval_loss_per_epoch)
+        # train_loss_per_lr.append(train_loss_per_epoch)
+        # eval_loss_per_lr.append(eval_loss_per_epoch)
         
         #saving calculated weights
         torch.save(fcn.state_dict(), os.path.join(currentdir, savingfile))
