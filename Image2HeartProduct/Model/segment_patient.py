84c9a06c31f098e974e2f1b8e6497304069d041c
levijn <levijndejager@gmail.com>
2022-01-17 12:49:22 +0100

    readme

--- Image2HeartProduct/Model/segment_patient.py
+++ Image2HeartProduct/Model/segment_patient.py
@@ -10,24 +10,14 @@ import config
 from to_image import run_model_rtrn_results, convert_to_segmented_imgs
 from patientdataset import get_patient_dataloader
 from voxelplot import create_voxelplot_from_results
-from slicedataset import (RandomZoom,
-                          PadImage,
-                          SudoRGB,
-                          ToTensor,
-                          Normalizer)
-
-
-
-    
 
 
 def main():
-    patient = "097"
+    patient = "097"     # Always ues a 3 digit number ('002', '045', etc.)
     img_path = config.simpledata_dir / f"patient{patient}_frame01.nii.gz"
     lbl_path = config.simpledata_dir / f"patient{patient}_frame01_gt.nii.gz"
     
     patient_dataloader = get_patient_dataloader(img_path, lbl_path)
-    
     
     patient_batch = None
     for batch in patient_dataloader:
@@ -37,24 +27,6 @@ def main():
     result_images = convert_to_segmented_imgs(results)
     create_voxelplot_from_results(result_images)
     
-    # create_3d_scatterplot(results)
-    # create_3d_scatterplot_labels(labels)
-
-    
-    
-    # fig = plt.figure(1)
-    # ax1 = fig.add_subplot(1,5,1)
-    # ax1.imshow(F.to_pil_image(slice_result[0,:,:]))
-    # ax2 = fig.add_subplot(1,5,2)
-    # ax2.imshow(F.to_pil_image(slice_result[1,:,:]))
-    # ax3 = fig.add_subplot(1,5,3)
-    # ax3.imshow(F.to_pil_image(slice_result[2,:,:]))
-    # ax4 = fig.add_subplot(1,5,4)
-    # ax4.imshow(F.to_pil_image(slice_result[3,:,:]))
-    # ax5 = fig.add_subplot(1,5,5)
-    # ax5.imshow(F.to_pil_image(slice_input), cmap="gray")
-    # plt.show()
-
 
 if __name__ == '__main__':
     main()
---
+++ Image2HeartProduct/README.md
@@ -0,0 +1,11 @@
+# Image to Heart 
+
+
+## Introduction
+
+
+## Setting up project
+To get the project working you have to run preprocess.py. 
+
+
+##
